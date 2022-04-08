a0b048b01230c9ed109c6fa5584b5d67f7148377
juhay-git <juha.yli-hemminki@seamk.fi>
2022-04-08 16:33:03 +0300

    Portti korjattu

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
-var HTTP_PORT = 8000
+var HTTP_PORT = process.env.PORT || 8080
 // Käynnistetään serveri
 app.listen(HTTP_PORT, () =>{
     console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
